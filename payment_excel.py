a21d46614d0f712a2f0c0c0b54f0cbf4962b40a3
DongJunHan <gamedokdok@gmail.com>
2021-02-11 12:32:36 +0900

    데이터파싱하는 함수 추가.

--- payment_excel.py
+++ payment_excel.py
@@ -8,7 +8,30 @@ def get_excel_data(path):
     #시트 이름으로 불러오기
     load_ws = load_wb['Sheet1']
 
-    #A2 cell의 값을 가져오기.
-    print(load_ws['A2'].value)
+    #cell의 데이터 가져오기.
+    payment_list =[]
+    for row in load_ws.rows:
+        row_list = []
+        for cell in row:
+            row_list.append(cell.value)
+        payment_list.append(row_list)
+    return payment_list
 
-get_excel_data(path)
+def parsing_excel_data(data_list):
+    cell_list = []
+    for data in data_list:
+        date = data[0]
+        if date != "승인일":
+            cell = []
+            cell.append(data[0]) # date
+            cell.append(data[1]) # time 내용을 점심식사, 저녁식사구분하기 위함.
+            cell.append(data[6]) # shop name
+            cell.append(data[7]) # shop 식대인지, 택시인지 구분하기 위함. TODO. 현재는 식대랑, 택시만 사용할거같지만 추후에 어떤것이 더 생길지 모름
+            cell.append(data[10]) # price
+            cell_list.append(cell)
+    return cell_list
+
+
+data_list = get_excel_data(path)
+parse_data = parsing_excel_data(data_list)
+print(parse_data)
