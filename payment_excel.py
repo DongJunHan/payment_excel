75fb00486e6fbd437df27ebd7f1243fe869b61c9
DongJunHan <gamedokdok@gmail.com>
2021-02-11 11:08:56 +0900

    엑셀 로드해서 데이터 가지고오는 로직 함수화

--- payment_excel.py
+++ payment_excel.py
@@ -1,11 +1,14 @@
 from openpyxl import load_workbook
 
+path = "./한동준_국민체크8906.xlsx"
+def get_excel_data(path):
+    #시트 로드. data_only=True로 해줘야 수식이 아닌 값으로 받아온다.
+    load_wb = load_workbook(path,data_only=True)
 
-#시트 로드. data_only=True로 해줘야 수식이 아닌 값으로 받아온다.
-load_wb = load_workbook("./한동준_국민체크8906.xlsx",data_only=True)
+    #시트 이름으로 불러오기
+    load_ws = load_wb['Sheet1']
 
-#시트 이름으로 불러오기
-load_ws = load_wb['Sheet1']
+    #A2 cell의 값을 가져오기.
+    print(load_ws['A2'].value)
 
-#A2 cell의 값을 가져오기.
-print(load_ws['A2'].value)
+get_excel_data(path)
