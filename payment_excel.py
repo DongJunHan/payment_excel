b8bb4931c91e64e3ec4edb94463c5b30c1f7a2ca
DongJunHan <gamedokdok@gmail.com>
2021-02-11 12:51:25 +0900

    엑셀쓰기함수 생성, 파일읽을때 예외처리 추가.

--- payment_excel.py
+++ payment_excel.py
@@ -1,9 +1,15 @@
 from openpyxl import load_workbook
+# from openpyxl import Workbook
 
 path = "./한동준_국민체크8906.xlsx"
+write_path = "./OOO_법인카드지출내역서1.xlsx"
 def get_excel_data(path):
     #시트 로드. data_only=True로 해줘야 수식이 아닌 값으로 받아온다.
-    load_wb = load_workbook(path,data_only=True)
+    try:
+        load_wb = load_workbook(path,data_only=True)
+    except FileNotFoundError:
+        print("get_excel_data There is no file")
+        return
 
     #시트 이름으로 불러오기
     load_ws = load_wb['Sheet1']
@@ -31,7 +37,17 @@ def parsing_excel_data(data_list):
             cell_list.append(cell)
     return cell_list
 
+def write_excel(path,data_list):
+    try:
+        load_wb = load_workbook(path,data_only=True)
+    except FileNotFoundError:
+        print("write_excel There is no file")
+        return
+    sheet1 = load_wb.active
+
+    # write_wb = Workbook()
 
 data_list = get_excel_data(path)
 parse_data = parsing_excel_data(data_list)
 print(parse_data)
+write_excel(write_path,parse_data)
