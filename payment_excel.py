064c054cf8bd6735ab87df430a8838e86618c986
DongJunHan <gamedokdok@gmail.com>
2021-02-12 12:25:17 +0900

    완성

--- payment_excel.py
+++ payment_excel.py
@@ -1,31 +1,32 @@
 from openpyxl import load_workbook
+from operator import itemgetter
 # from openpyxl import Workbook
 
 path = "./한동준_국민체크8906.xlsx"
-write_path = "./OOO_법인카드지출내역서1.xlsx"
+writePath = "./OOO_법인카드지출내역서.xlsx"
 def get_excel_data(path):
     #시트 로드. data_only=True로 해줘야 수식이 아닌 값으로 받아온다.
     try:
-        load_wb = load_workbook(path,data_only=True)
+        loadWb = load_workbook(path,data_only=True)
     except FileNotFoundError:
         print("get_excel_data There is no file")
         return
 
     #시트 이름으로 불러오기
-    load_ws = load_wb['Sheet1']
+    loadWs = loadWb['Sheet1']
 
     #cell의 데이터 가져오기.
-    payment_list =[]
-    for row in load_ws.rows:
-        row_list = []
+    paymentList =[]
+    for row in loadWs.rows:
+        rowList = []
         for cell in row:
-            row_list.append(cell.value)
-        payment_list.append(row_list)
-    return payment_list
+            rowList.append(cell.value)
+        paymentList.append(rowList)
+    return paymentList
 
-def parsing_excel_data(data_list):
-    cell_list = []
-    for data in data_list:
+def parsing_excel_data(dataList):
+    cellList = []
+    for data in dataList:
         date = data[0]
         if date != "승인일":
             cell = []
@@ -34,20 +35,60 @@ def parsing_excel_data(data_list):
             cell.append(data[6]) # shop name
             cell.append(data[7]) # shop 식대인지, 택시인지 구분하기 위함. TODO. 현재는 식대랑, 택시만 사용할거같지만 추후에 어떤것이 더 생길지 모름
             cell.append(data[10]) # price
-            cell_list.append(cell)
-    return cell_list
+            cellList.append(cell)
+    return cellList
 
-def write_excel(path,data_list):
+def write_excel(path,dataList):
+    redataList = sorted(dataList, key=itemgetter(0))
+    print(redataList)
     try:
-        load_wb = load_workbook(path,data_only=True)
+        loadWb = load_workbook(path,data_only=False)
+        sheet = loadWb.get_sheet_by_name('고정비')
+        # sheet = loadWb.get_active_sheet()
+        rowNum = 6
+        totalAmount = 0
+        for row in redataList:
+            columnNum = 2
+            #승인일자
+            sheet.cell(row = rowNum, column = columnNum, value = row[0])
+            columnNum += 1
+            #상호명
+            sheet.cell(row = rowNum, column = columnNum, value = row[2])
+            columnNum += 1
+            #금액
+            sheet.cell(row = rowNum, column = columnNum, value = row[4])
+            totalAmount += int(row[4])
+            columnNum += 1
+            #업무
+            if row[3] == "택시":
+                sheet.cell(row = rowNum, column = columnNum, value = "OmniDoc")
+                columnNum += 1
+                #내용
+                sheet.cell(row = rowNum, column = columnNum, value = row[3])
+            else:
+                sheet.cell(row = rowNum, column = columnNum, value = "공통비")
+                columnNum += 1
+                #내용
+                if (row[1][0:2].find('11') != -1) or (row[1][0:2].find('12') != -1) or (row[1][0:2].find('13') != -1) or (row[1][0:2].find('14') != -1):
+                    sheet.cell(row = rowNum, column = columnNum, value = "점심식사")
+                elif (row[1][0:2].find('17') != -1) or (row[1][0:2].find('18') != -1) or (row[1][0:2].find('19') != -1) or (row[1][0:2].find('20') != -1):
+                    sheet.cell(row = rowNum, column = columnNum, value = "저녁식사")
+                else:
+                    sheet.cell(row = rowNum, column = columnNum, value = "식대")
+
+            
+
+            # for data in row:
+            #     sheet.cell(row = rowNum, column = columnNum, value = data)
+            #     columnNum += 1
+            rowNum += 1
+        sheet.cell(row = 3, column = 4, value = totalAmount)
     except FileNotFoundError:
         print("write_excel There is no file")
         return
-    sheet1 = load_wb.active
+    loadWb.save(path)
 
-    # write_wb = Workbook()
-
-data_list = get_excel_data(path)
-parse_data = parsing_excel_data(data_list)
-print(parse_data)
-write_excel(write_path,parse_data)
+dataList = get_excel_data(path)
+parseData = parsing_excel_data(dataList)
+# print(parseData)
+write_excel(writePath,parseData)
